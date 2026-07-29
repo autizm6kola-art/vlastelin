73b85acb5b2ac1d0615021bccb6f71f12c0bb6ed
Светлана <autizm6kola@yandex.com>
2026-07-29 09:58:57 +0300

    800

--- src/components/DailyProgress.js
+++ src/components/DailyProgress.js
@@ -25,7 +25,7 @@ function DailyProgress() {
 
   return (
     <p style={{ marginTop: "10px", fontWeight: "bold" }}>
-       ЦЕЛЬ: 700 слов. 📅 Прочитано:  {todayCount} слов
+       ЦЕЛЬ: 800 слов. 📅 Прочитано:  {todayCount} слов
     </p>
   );
 }
